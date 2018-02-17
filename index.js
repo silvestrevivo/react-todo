177369279cffd7a2aa3f20c1066392b5093f5031
silvestrevivo <silvestrevivomillan@gmail.com>
2018-02-17 09:36:42 +0100

    eslint integration

--- index.js
+++
@@ -1,6 +0,0 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import App from './src/components/app';
-import './assets/scss/style.scss';
-
-ReactDOM.render(<App />, document.getElementById('root'));
